db59e2b79a241934dd707c879d398f7c6017abf2
NabilAslam <nabil.aslam26@gmail.com>
2023-05-27 12:43:00 +0200

    "changes"

--- routes/transition.routes.js
+++ routes/transition.routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 
-/*
+
 router.get("/", (req, res, next) => {
     res.json("home page");
 });
-*/
+
 
 module.exports = router;
