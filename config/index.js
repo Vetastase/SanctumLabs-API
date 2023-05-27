cb363d249829b73bd3802085eb28c88db578ded7
NabilAslam <nabil.aslam26@gmail.com>
2023-05-27 12:25:38 +0200

    "changes"

--- config/index.js
+++ config/index.js
@@ -24,7 +24,8 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: [FRONTEND_URL],
+      'Access-Control-Allow-Origin': [FRONTEND_URL] 
     })
   );
 
