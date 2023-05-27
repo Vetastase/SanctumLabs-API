f0745a7e0144850157c50ea02704a4a09b0f19fc
Nabil Aslam <118678544+Vetastase@users.noreply.github.com>
2023-05-27 10:40:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 // node server.js testing
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 
+https://calm-gray-sawfish-tie.cyclic.app;
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
