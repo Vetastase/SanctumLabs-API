8bc3c3568b3726cc0142a9b3a46c35d8ecdfb27c
NabilAslam <nabil.aslam26@gmail.com>
2023-05-27 11:17:14 +0200

    "changes"

--- routes/transition.routes.js
+++ routes/transition.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 
+/*
 router.get("/", (req, res, next) => {
     res.json("home page");
-  });
+});
+*/
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const app = require("./app");
 // node server.js testing
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 
-https://calm-gray-sawfish-tie.cyclic.app;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
